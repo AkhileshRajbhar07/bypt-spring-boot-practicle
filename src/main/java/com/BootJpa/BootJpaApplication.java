aa8b8d1557f42c14a0ad7eebfb4266abbb367593
Akhil Rajbhar <77782906+AkhileshRajbhar07@users.noreply.github.com>
2021-02-02 14:13:20 +0530

    small change

--- src/main/java/com/BootJpa/BootJpaApplication.java
+++ src/main/java/com/BootJpa/BootJpaApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BootJpaApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(BootJpaApplication.class, args);
 	}
